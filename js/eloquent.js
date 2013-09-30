8064c0f771987e5f528c709a74241446b656a60a
Erin Millard <ezzatron@gmail.com>
2013-09-30 22:16:19 +1000

    Ideas.

--- js/eloquent.js
+++ js/eloquent.js
@@ -1,8 +1,15 @@
-jQuery(document).ready(function() {
-    jQuery("[data-toggle='tooltip']").tooltip(
-        {
-            "placement": "bottom",
-            "container": "body"
-        }
-    );
-});
+jQuery(document).ready(
+    function() {
+        jQuery("[data-toggle='tooltip']").tooltip(
+            {
+                "placement": "bottom",
+                "container": "body"
+            }
+        );
+        jQuery('pre code').each(
+            function(i, element) {
+                hljs.highlightBlock(element);
+            }
+        );
+    }
+);
