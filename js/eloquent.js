b718929e89d733b56764d13eba711fd66265bc9b
Erin Millard <ezzatron@gmail.com>
2016-01-15 14:21:02 +1000

    Revamp.

--- js/eloquent.js
+++ js/eloquent.js
@@ -1,8 +1,27 @@
-jQuery(document).ready(function() {
-    jQuery("[data-toggle='tooltip']").tooltip(
-        {
-            "placement": "bottom",
-            "container": "body"
+"use strict";
+
+// the token is read only, so calm your tits
+var github = new GitHub({token: '29616f2628c43b5156bdd928cd8b30da539d2972'});
+var githubGet = Promise.promisify(github.get, {context: github});
+
+Promise.join(
+    githubGet('orgs/eloquent/repos?type=public', {all: true}),
+    githubGet('users/ezzatron/starred', {all: true}),
+
+    function (repositories, stars) {
+        var repositoryNames = {};
+        var starredRepositories = [];
+
+        for (let star of stars) {
+           repositoryNames[star.full_name] = true;
         }
-    );
-});
+
+        for (let repository of repositories) {
+            if (repositoryNames[repository.full_name]) {
+                starredRepositories.push(repository);
+            }
+        }
+
+        riot.mount('github-repository-list', {repositories: starredRepositories});
+    }
+);
---
+++ tag/github-repository-list.tag
@@ -0,0 +1,25 @@
+<github-repository-list>
+    <ul>
+        <li class="repository" each={repositories}>
+            <strong><a href="{homepage ? homepage : html_url}">{name}</a></strong>: {description}
+        </li>
+    </ul>
+
+    <script>
+        opts.repositories.sort(
+            function (left, right) {
+                if (left.stargazers_count < right.stargazers_count) {
+                    return 1;
+                }
+
+                if (left.stargazers_count > right.stargazers_count) {
+                    return -1;
+                }
+
+                return 0;
+            }
+        );
+
+        this.repositories = opts.repositories;
+    </script>
+</github-repository-list>
